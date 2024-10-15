00915a086024d06ee82df7efb4bdb369be59092d
Joppe Nagtzaam <j.l.j.nagtzaam@student.tue.nl>
2024-10-15 16:35:21 +0200

    Add Assets folder

---
+++ Assets/Test/WaypointMover.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointMover : MonoBehaviour
+{
+    // stores reference to the waypoint system this object will use.
+    [SerializeField] private Waypoints waypoints;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float distanceThreshold = 0.1f;
+
+    // the current waypoint target that the object is moving towards
+    private Transform currentWaypoint;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set initial position to the first waypoint
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.position = currentWaypoint.position;
+
+        // set the next waypoint target
+        currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+        transform.LookAt(currentWaypoint);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, currentWaypoint.position, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, currentWaypoint.position) < distanceThreshold)
+        {
+            currentWaypoint = waypoints.GetNextWaypoint(currentWaypoint);
+            transform.LookAt(currentWaypoint);
+        }
+    }
+}
